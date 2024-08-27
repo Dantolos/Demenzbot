0280f6ee614a667ffe8ddf71f03e62aa99870991
Dantolos <45291569+Dantolos@users.noreply.github.com>
2024-08-27 09:42:17 +0200

    1.0.1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Demenzbot
+
+### by demenzworld
---
+++ demenzbot.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: DemenzBot
+ * Plugin URI: https://github.com/Dantolos/Demenzbot
+ * Description: GPT Chat Block
+ * Version: 1.0.0
+ * Author: Aaron Giaimo 
+ * Text Domain: demenzbot 
+ * Requires PHP: 5.6.20
+ * Tested up to: 6.1.1
+ * License: GPLv2 or later
+ * 
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+
